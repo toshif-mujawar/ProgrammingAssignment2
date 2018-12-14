24f8df587e0b3b4b5c27ae2e3cbf5688dde6de28
toshif-mujawar <45873042+toshif-mujawar@users.noreply.github.com>
2018-12-14 17:41:58 +0530

    Add files via upload
    Week 3 Peer Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creating a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invMatrix <- NULL
+        
+        #set the value of the Matrix
+        setMatrix <- function(y) {
+                x <<- y
+                invMatrix <<- NULL
+        }
+        
+        getMatrix <- function() x                              #get the value of the Matrix
+        setInverse <- function(inverse) invMatrix <<- inverse  #set the value of the invertible matrix
+        getInverse <- function() invMatrix                     #get the value of the invertible matrix
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## computing the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
+        
+        #get the value of the invertible matrix from the makeCacheMatrix function
+        invMatrix <- x$getInverse()
+        if(!is.null(invMatrix)) {                       #if inverse matrix is not NULL
+                message("Getting Cached Invertible Matrix")   #Type message: Getting Cached Invertible Matrix 
+                return(invMatrix)                             #return the invertible matrix
+        }
+        
+        #if value of the invertible matrix is NULL then  
+        MatrixData <- x$getMatrix()                     #get the original Matrix Data 
+        invMatrix <- solve(MatrixData, ...)             #use solve function to inverse the matrix
+        x$setInverse(invMatrix)                         #set the invertible matrix 
+        return(invMatrix)                               #return the invertible matrix
         ## Return a matrix that is the inverse of 'x'
 }
+
+#####Testing#####
+
+TestMatrix <- matrix(1:4,2,2)
+TestMatrix
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+cacheSolve(CacheMatrix)
+
+TestMatrix2 <- matrix(c(0,1,1,0,0,1,1,0,1),3,3)
+TestMatrix2
+CacheMatrix <- makeCacheMatrix(TestMatrix2)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+cacheSolve(CacheMatrix)
+
